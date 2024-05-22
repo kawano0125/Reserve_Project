8f86d8e4972dd5de98f7017de3a1cf446160274a
tounichi <satou-ym@tounichi-g.co.jp>
2024-05-22 11:36:31 +0900

    a

--- Project3/AllRoomForm.h
+++ Project3/AllRoomForm.h
@@ -37,6 +37,21 @@ namespace Project3 {
 	private: System::Windows::Forms::Button^ b_Search;
 	private: System::Windows::Forms::Button^ b_Mydata;
 	private: System::Windows::Forms::Button^ b_Logout;
+	private: System::Windows::Forms::Button^ b_Room1;
+	private: System::Windows::Forms::Button^ b_Room2;
+	private: System::Windows::Forms::Button^ b_Room4;
+	private: System::Windows::Forms::Button^ b_Room3;
+	private: System::Windows::Forms::Button^ b_Room6;
+
+
+
+
+
+
+	private: System::Windows::Forms::Button^ b_Room5;
+
+
+
 
 
 	protected:
@@ -59,6 +74,12 @@ namespace Project3 {
 			this->b_Search = (gcnew System::Windows::Forms::Button());
 			this->b_Mydata = (gcnew System::Windows::Forms::Button());
 			this->b_Logout = (gcnew System::Windows::Forms::Button());
+			this->b_Room1 = (gcnew System::Windows::Forms::Button());
+			this->b_Room2 = (gcnew System::Windows::Forms::Button());
+			this->b_Room4 = (gcnew System::Windows::Forms::Button());
+			this->b_Room3 = (gcnew System::Windows::Forms::Button());
+			this->b_Room6 = (gcnew System::Windows::Forms::Button());
+			this->b_Room5 = (gcnew System::Windows::Forms::Button());
 			this->SuspendLayout();
 			// 
 			// b_Search
@@ -112,14 +133,125 @@ namespace Project3 {
 			this->b_Logout->Text = L"ƒƒOƒAƒEƒg";
 			this->b_Logout->UseVisualStyleBackColor = true;
 			// 
+			// b_Room1
+			// 
+			this->b_Room1->FlatAppearance->BorderColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(255)),
+				static_cast<System::Int32>(static_cast<System::Byte>(192)), static_cast<System::Int32>(static_cast<System::Byte>(192)));
+			this->b_Room1->FlatAppearance->MouseDownBackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(255)),
+				static_cast<System::Int32>(static_cast<System::Byte>(128)), static_cast<System::Int32>(static_cast<System::Byte>(128)));
+			this->b_Room1->FlatAppearance->MouseOverBackColor = System::Drawing::Color::Red;
+			this->b_Room1->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->b_Room1->Font = (gcnew System::Drawing::Font(L"‚l‚r ‚o–¾’©", 15.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(128)));
+			this->b_Room1->Location = System::Drawing::Point(50, 170);
+			this->b_Room1->Name = L"b_Room1";
+			this->b_Room1->Size = System::Drawing::Size(200, 70);
+			this->b_Room1->TabIndex = 3;
+			this->b_Room1->Text = L"Room1";
+			this->b_Room1->UseVisualStyleBackColor = true;
+			// 
+			// b_Room2
+			// 
+			this->b_Room2->FlatAppearance->BorderColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(255)),
+				static_cast<System::Int32>(static_cast<System::Byte>(224)), static_cast<System::Int32>(static_cast<System::Byte>(192)));
+			this->b_Room2->FlatAppearance->MouseDownBackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(255)),
+				static_cast<System::Int32>(static_cast<System::Byte>(192)), static_cast<System::Int32>(static_cast<System::Byte>(128)));
+			this->b_Room2->FlatAppearance->MouseOverBackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(255)),
+				static_cast<System::Int32>(static_cast<System::Byte>(128)), static_cast<System::Int32>(static_cast<System::Byte>(0)));
+			this->b_Room2->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->b_Room2->Font = (gcnew System::Drawing::Font(L"‚l‚r ‚o–¾’©", 15.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(128)));
+			this->b_Room2->Location = System::Drawing::Point(350, 170);
+			this->b_Room2->Name = L"b_Room2";
+			this->b_Room2->Size = System::Drawing::Size(200, 70);
+			this->b_Room2->TabIndex = 4;
+			this->b_Room2->Text = L"Room2";
+			this->b_Room2->UseVisualStyleBackColor = true;
+			// 
+			// b_Room4
+			// 
+			this->b_Room4->FlatAppearance->BorderColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(192)),
+				static_cast<System::Int32>(static_cast<System::Byte>(255)), static_cast<System::Int32>(static_cast<System::Byte>(255)));
+			this->b_Room4->FlatAppearance->MouseDownBackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(128)),
+				static_cast<System::Int32>(static_cast<System::Byte>(255)), static_cast<System::Int32>(static_cast<System::Byte>(255)));
+			this->b_Room4->FlatAppearance->MouseOverBackColor = System::Drawing::Color::Aqua;
+			this->b_Room4->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->b_Room4->Font = (gcnew System::Drawing::Font(L"‚l‚r ‚o–¾’©", 15.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(128)));
+			this->b_Room4->Location = System::Drawing::Point(350, 290);
+			this->b_Room4->Name = L"b_Room4";
+			this->b_Room4->Size = System::Drawing::Size(200, 70);
+			this->b_Room4->TabIndex = 6;
+			this->b_Room4->Text = L"Room4";
+			this->b_Room4->UseVisualStyleBackColor = true;
+			// 
+			// b_Room3
+			// 
+			this->b_Room3->FlatAppearance->BorderColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(255)),
+				static_cast<System::Int32>(static_cast<System::Byte>(255)), static_cast<System::Int32>(static_cast<System::Byte>(192)));
+			this->b_Room3->FlatAppearance->MouseDownBackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(255)),
+				static_cast<System::Int32>(static_cast<System::Byte>(255)), static_cast<System::Int32>(static_cast<System::Byte>(128)));
+			this->b_Room3->FlatAppearance->MouseOverBackColor = System::Drawing::Color::Yellow;
+			this->b_Room3->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->b_Room3->Font = (gcnew System::Drawing::Font(L"‚l‚r ‚o–¾’©", 15.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(128)));
+			this->b_Room3->Location = System::Drawing::Point(50, 290);
+			this->b_Room3->Name = L"b_Room3";
+			this->b_Room3->Size = System::Drawing::Size(200, 70);
+			this->b_Room3->TabIndex = 5;
+			this->b_Room3->Text = L"Room3";
+			this->b_Room3->UseVisualStyleBackColor = true;
+			// 
+			// b_Room6
+			// 
+			this->b_Room6->FlatAppearance->BorderColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(255)),
+				static_cast<System::Int32>(static_cast<System::Byte>(192)), static_cast<System::Int32>(static_cast<System::Byte>(255)));
+			this->b_Room6->FlatAppearance->MouseDownBackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(255)),
+				static_cast<System::Int32>(static_cast<System::Byte>(128)), static_cast<System::Int32>(static_cast<System::Byte>(255)));
+			this->b_Room6->FlatAppearance->MouseOverBackColor = System::Drawing::Color::Fuchsia;
+			this->b_Room6->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->b_Room6->Font = (gcnew System::Drawing::Font(L"‚l‚r ‚o–¾’©", 15.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(128)));
+			this->b_Room6->Location = System::Drawing::Point(350, 410);
+			this->b_Room6->Name = L"b_Room6";
+			this->b_Room6->Size = System::Drawing::Size(200, 70);
+			this->b_Room6->TabIndex = 8;
+			this->b_Room6->Text = L"Room6";
+			this->b_Room6->UseVisualStyleBackColor = true;
+			// 
+			// b_Room5
+			// 
+			this->b_Room5->FlatAppearance->BorderColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(192)),
+				static_cast<System::Int32>(static_cast<System::Byte>(192)), static_cast<System::Int32>(static_cast<System::Byte>(255)));
+			this->b_Room5->FlatAppearance->MouseDownBackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(128)),
+				static_cast<System::Int32>(static_cast<System::Byte>(128)), static_cast<System::Int32>(static_cast<System::Byte>(255)));
+			this->b_Room5->FlatAppearance->MouseOverBackColor = System::Drawing::Color::Blue;
+			this->b_Room5->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->b_Room5->Font = (gcnew System::Drawing::Font(L"‚l‚r ‚o–¾’©", 15.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(128)));
+			this->b_Room5->Location = System::Drawing::Point(50, 410);
+			this->b_Room5->Name = L"b_Room5";
+			this->b_Room5->Size = System::Drawing::Size(200, 70);
+			this->b_Room5->TabIndex = 7;
+			this->b_Room5->Text = L"Room5";
+			this->b_Room5->UseVisualStyleBackColor = true;
+			// 
 			// AllRoomForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 12);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(584, 561);
+			this->Controls->Add(this->b_Room6);
+			this->Controls->Add(this->b_Room5);
+			this->Controls->Add(this->b_Room4);
+			this->Controls->Add(this->b_Room3);
+			this->Controls->Add(this->b_Room2);
+			this->Controls->Add(this->b_Room1);
 			this->Controls->Add(this->b_Logout);
 			this->Controls->Add(this->b_Mydata);
 			this->Controls->Add(this->b_Search);
+			this->MaximizeBox = false;
+			this->MinimizeBox = false;
 			this->Name = L"AllRoomForm";
 			this->Text = L"AllRoomForm";
 			this->ResumeLayout(false);
