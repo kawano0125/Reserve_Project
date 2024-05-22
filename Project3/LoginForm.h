f18b7f2cca3cc4108012e435bfe8dd7fbbea73e0
米田真由香 <yoneda-my@tounichi-g.co.jp>
2024-05-22 10:42:52 +0900

    画面設計しました　米田

--- Project3/LoginForm.h
+++ Project3/LoginForm.h
@@ -40,6 +40,20 @@ namespace Project3 {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::Label^ l_UserName;
+	private: System::Windows::Forms::Label^ l_UserPassword;
+	private: System::Windows::Forms::TextBox^ tb_UserName;
+	private: System::Windows::Forms::TextBox^ tb_UserPassword;
+	private: System::Windows::Forms::Button^ b_AllRoom;
+	private: System::Windows::Forms::Button^ b_NewEntry;
+	private: System::Windows::Forms::Button^ b_Login;
+
+
+
+
+
+	protected:
+
 
 	private:
 		/// <summary>
@@ -54,16 +68,100 @@ namespace Project3 {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			this->l_UserName = (gcnew System::Windows::Forms::Label());
+			this->l_UserPassword = (gcnew System::Windows::Forms::Label());
+			this->tb_UserName = (gcnew System::Windows::Forms::TextBox());
+			this->tb_UserPassword = (gcnew System::Windows::Forms::TextBox());
+			this->b_AllRoom = (gcnew System::Windows::Forms::Button());
+			this->b_NewEntry = (gcnew System::Windows::Forms::Button());
+			this->b_Login = (gcnew System::Windows::Forms::Button());
 			this->SuspendLayout();
+			// 
+			// l_UserName
+			// 
+			this->l_UserName->AutoSize = true;
+			this->l_UserName->Font = (gcnew System::Drawing::Font(L"ＭＳ ゴシック", 9, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(128)));
+			this->l_UserName->Location = System::Drawing::Point(60, 194);
+			this->l_UserName->Name = L"l_UserName";
+			this->l_UserName->Size = System::Drawing::Size(109, 15);
+			this->l_UserName->TabIndex = 0;
+			this->l_UserName->Text = L"ユーザー名：";
+			// 
+			// l_UserPassword
+			// 
+			this->l_UserPassword->AutoSize = true;
+			this->l_UserPassword->Font = (gcnew System::Drawing::Font(L"MS UI Gothic", 9, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(128)));
+			this->l_UserPassword->Location = System::Drawing::Point(91, 277);
+			this->l_UserPassword->Name = L"l_UserPassword";
+			this->l_UserPassword->Size = System::Drawing::Size(78, 15);
+			this->l_UserPassword->TabIndex = 1;
+			this->l_UserPassword->Text = L"パスワード：";
+			// 
+			// tb_UserName
+			// 
+			this->tb_UserName->Location = System::Drawing::Point(209, 187);
+			this->tb_UserName->Name = L"tb_UserName";
+			this->tb_UserName->Size = System::Drawing::Size(308, 22);
+			this->tb_UserName->TabIndex = 2;
+			// 
+			// tb_UserPassword
+			// 
+			this->tb_UserPassword->Location = System::Drawing::Point(209, 270);
+			this->tb_UserPassword->Name = L"tb_UserPassword";
+			this->tb_UserPassword->Size = System::Drawing::Size(308, 22);
+			this->tb_UserPassword->TabIndex = 3;
+			// 
+			// b_AllRoom
+			// 
+			this->b_AllRoom->Font = (gcnew System::Drawing::Font(L"MS UI Gothic", 9, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(128)));
+			this->b_AllRoom->Location = System::Drawing::Point(274, 50);
+			this->b_AllRoom->Name = L"b_AllRoom";
+			this->b_AllRoom->Size = System::Drawing::Size(114, 51);
+			this->b_AllRoom->TabIndex = 4;
+			this->b_AllRoom->Text = L"研修室表示";
+			this->b_AllRoom->UseVisualStyleBackColor = true;
+			// 
+			// b_NewEntry
+			// 
+			this->b_NewEntry->Font = (gcnew System::Drawing::Font(L"MS UI Gothic", 9, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(128)));
+			this->b_NewEntry->Location = System::Drawing::Point(403, 51);
+			this->b_NewEntry->Name = L"b_NewEntry";
+			this->b_NewEntry->Size = System::Drawing::Size(114, 50);
+			this->b_NewEntry->TabIndex = 5;
+			this->b_NewEntry->Text = L"新規作成";
+			this->b_NewEntry->UseVisualStyleBackColor = true;
+			// 
+			// b_Login
+			// 
+			this->b_Login->Font = (gcnew System::Drawing::Font(L"MS UI Gothic", 10.8F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(128)));
+			this->b_Login->Location = System::Drawing::Point(198, 396);
+			this->b_Login->Name = L"b_Login";
+			this->b_Login->Size = System::Drawing::Size(189, 64);
+			this->b_Login->TabIndex = 6;
+			this->b_Login->Text = L"ログイン";
+			this->b_Login->UseVisualStyleBackColor = true;
 			// 
 			// LoginForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(8, 15);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(282, 253);
+			this->ClientSize = System::Drawing::Size(582, 553);
+			this->Controls->Add(this->b_Login);
+			this->Controls->Add(this->b_NewEntry);
+			this->Controls->Add(this->b_AllRoom);
+			this->Controls->Add(this->tb_UserPassword);
+			this->Controls->Add(this->tb_UserName);
+			this->Controls->Add(this->l_UserPassword);
+			this->Controls->Add(this->l_UserName);
 			this->Name = L"LoginForm";
 			this->Text = L"LoginForm";
 			this->ResumeLayout(false);
+			this->PerformLayout();
 
 		}
 #pragma endregion
