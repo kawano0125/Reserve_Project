03ae6bfe376f405ffc6c937340b231dfc1777a7f
tounichi <satou-ym@tounichi-g.co.jp>
2024-05-22 12:03:14 +0900

    q

--- Project3/RoomDatailForm.h
+++ Project3/RoomDatailForm.h
@@ -34,6 +34,18 @@ namespace Project3 {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::Button^ b_RoomdataReturn;
+	private: System::Windows::Forms::Button^ b_Reserve;
+	protected:
+
+	private: System::Windows::Forms::Label^ l_Roomdata;
+	private: System::Windows::Forms::ListBox^ lb_ReserveState;
+
+
+	private: System::Windows::Forms::DateTimePicker^ dtp_Calendar;
+
+
+	protected:
 
 	private:
 		/// <summary>
@@ -48,12 +60,75 @@ namespace Project3 {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"RoomDatailForm";
-			this->Padding = System::Windows::Forms::Padding(0);
+			this->b_RoomdataReturn = (gcnew System::Windows::Forms::Button());
+			this->b_Reserve = (gcnew System::Windows::Forms::Button());
+			this->l_Roomdata = (gcnew System::Windows::Forms::Label());
+			this->lb_ReserveState = (gcnew System::Windows::Forms::ListBox());
+			this->dtp_Calendar = (gcnew System::Windows::Forms::DateTimePicker());
+			this->SuspendLayout();
+			// 
+			// b_RoomdataReturn
+			// 
+			this->b_RoomdataReturn->Location = System::Drawing::Point(12, 23);
+			this->b_RoomdataReturn->Name = L"b_RoomdataReturn";
+			this->b_RoomdataReturn->Size = System::Drawing::Size(110, 40);
+			this->b_RoomdataReturn->TabIndex = 0;
+			this->b_RoomdataReturn->Text = L"–ß‚é";
+			this->b_RoomdataReturn->UseVisualStyleBackColor = true;
+			// 
+			// b_Reserve
+			// 
+			this->b_Reserve->Location = System::Drawing::Point(225, 526);
+			this->b_Reserve->Name = L"b_Reserve";
+			this->b_Reserve->Size = System::Drawing::Size(150, 23);
+			this->b_Reserve->TabIndex = 1;
+			this->b_Reserve->Text = L"—\–ñ";
+			this->b_Reserve->UseVisualStyleBackColor = true;
+			// 
+			// l_Roomdata
+			// 
+			this->l_Roomdata->AutoSize = true;
+			this->l_Roomdata->Location = System::Drawing::Point(63, 88);
+			this->l_Roomdata->Name = L"l_Roomdata";
+			this->l_Roomdata->Size = System::Drawing::Size(35, 12);
+			this->l_Roomdata->TabIndex = 2;
+			this->l_Roomdata->Text = L"label1";
+			this->l_Roomdata->Click += gcnew System::EventHandler(this, &RoomDatailForm::l_Roomdata_Click);
+			// 
+			// lb_ReserveState
+			// 
+			this->lb_ReserveState->FormattingEnabled = true;
+			this->lb_ReserveState->ItemHeight = 12;
+			this->lb_ReserveState->Location = System::Drawing::Point(65, 164);
+			this->lb_ReserveState->Name = L"lb_ReserveState";
+			this->lb_ReserveState->Size = System::Drawing::Size(470, 340);
+			this->lb_ReserveState->TabIndex = 3;
+			// 
+			// dtp_Calendar
+			// 
+			this->dtp_Calendar->Location = System::Drawing::Point(65, 139);
+			this->dtp_Calendar->Name = L"dtp_Calendar";
+			this->dtp_Calendar->Size = System::Drawing::Size(200, 19);
+			this->dtp_Calendar->TabIndex = 4;
+			// 
+			// RoomDatailForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 12);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(584, 561);
+			this->Controls->Add(this->dtp_Calendar);
+			this->Controls->Add(this->lb_ReserveState);
+			this->Controls->Add(this->l_Roomdata);
+			this->Controls->Add(this->b_Reserve);
+			this->Controls->Add(this->b_RoomdataReturn);
+			this->Name = L"RoomDatailForm";
+			this->Text = L"RoomDatailForm";
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
 		}
 #pragma endregion
-	};
+	private: System::Void l_Roomdata_Click(System::Object^ sender, System::EventArgs^ e) {
+	}
+};
 }
