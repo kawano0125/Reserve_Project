b61fa7280a7e6c7a2b7a9f75cdcf040e580d674d
高村圭吾 <takamura-ki@tounichi-g.co.jp>
2024-05-22 13:20:00 +0900

    #

--- Project3/SearchForm.h
+++ Project3/SearchForm.h
@@ -42,18 +42,23 @@ namespace Project3 {
 	private: System::Windows::Forms::Button^ b_SearchReturn;
 	private: System::Windows::Forms::ComboBox^ cb_StartHour;
 	private: System::Windows::Forms::ComboBox^ cb_StartMin;
+	private: System::Windows::Forms::ComboBox^ cb_EndHour;
+	private: System::Windows::Forms::ComboBox^ cb_EndMin;
+	private: System::Windows::Forms::TextBox^ tb_NumData;
 
 
 
 
 
-	private: System::Windows::Forms::ComboBox^ comboBox3;
-	private: System::Windows::Forms::ComboBox^ comboBox4;
-	private: System::Windows::Forms::TextBox^ textBox1;
+
+
+
 	private: System::Windows::Forms::Label^ l_Start;
 	private: System::Windows::Forms::Label^ l_End;
-	private: System::Windows::Forms::Label^ l_NumPeople;
 	private: System::Windows::Forms::Label^ l_Num;
+
+	private: System::Windows::Forms::Label^ l_NumData;
+
 
 
 
@@ -76,6 +81,7 @@ namespace Project3 {
 
 
 
+
 	private:
 		/// <summary>
 		/// 必要なデザイナー変数です。
@@ -93,13 +99,13 @@ namespace Project3 {
 			this->b_SearchReturn = (gcnew System::Windows::Forms::Button());
 			this->cb_StartHour = (gcnew System::Windows::Forms::ComboBox());
 			this->cb_StartMin = (gcnew System::Windows::Forms::ComboBox());
-			this->comboBox3 = (gcnew System::Windows::Forms::ComboBox());
-			this->comboBox4 = (gcnew System::Windows::Forms::ComboBox());
-			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->cb_EndHour = (gcnew System::Windows::Forms::ComboBox());
+			this->cb_EndMin = (gcnew System::Windows::Forms::ComboBox());
+			this->tb_NumData = (gcnew System::Windows::Forms::TextBox());
 			this->l_Start = (gcnew System::Windows::Forms::Label());
 			this->l_End = (gcnew System::Windows::Forms::Label());
-			this->l_NumPeople = (gcnew System::Windows::Forms::Label());
 			this->l_Num = (gcnew System::Windows::Forms::Label());
+			this->l_NumData = (gcnew System::Windows::Forms::Label());
 			this->l_EndMin = (gcnew System::Windows::Forms::Label());
 			this->l_StartMin = (gcnew System::Windows::Forms::Label());
 			this->l_StartHour = (gcnew System::Windows::Forms::Label());
@@ -110,10 +116,11 @@ namespace Project3 {
 			// 
 			// dtp_Calender
 			// 
-			this->dtp_Calender->Location = System::Drawing::Point(12, 192);
+			this->dtp_Calender->Location = System::Drawing::Point(12, 136);
 			this->dtp_Calender->Name = L"dtp_Calender";
 			this->dtp_Calender->Size = System::Drawing::Size(248, 22);
 			this->dtp_Calender->TabIndex = 0;
+			this->dtp_Calender->ValueChanged += gcnew System::EventHandler(this, &SearchForm::dtp_Calender_ValueChanged);
 			// 
 			// b_SearchReturn
 			// 
@@ -131,7 +138,7 @@ namespace Project3 {
 				L"9", L"10", L"11", L"12", L"13", L"14", L"15",
 					L"16", L"17", L"18"
 			});
-			this->cb_StartHour->Location = System::Drawing::Point(306, 192);
+			this->cb_StartHour->Location = System::Drawing::Point(290, 135);
 			this->cb_StartHour->Name = L"cb_StartHour";
 			this->cb_StartHour->Size = System::Drawing::Size(75, 23);
 			this->cb_StartHour->TabIndex = 3;
@@ -139,38 +146,39 @@ namespace Project3 {
 			// cb_StartMin
 			// 
 			this->cb_StartMin->FormattingEnabled = true;
-			this->cb_StartMin->Location = System::Drawing::Point(448, 192);
+			this->cb_StartMin->Items->AddRange(gcnew cli::array< System::Object^  >(2) { L"00", L"30" });
+			this->cb_StartMin->Location = System::Drawing::Point(448, 135);
 			this->cb_StartMin->Name = L"cb_StartMin";
 			this->cb_StartMin->Size = System::Drawing::Size(75, 23);
 			this->cb_StartMin->TabIndex = 4;
 			// 
-			// comboBox3
+			// cb_EndHour
 			// 
-			this->comboBox3->FormattingEnabled = true;
-			this->comboBox3->Location = System::Drawing::Point(306, 295);
-			this->comboBox3->Name = L"comboBox3";
-			this->comboBox3->Size = System::Drawing::Size(75, 23);
-			this->comboBox3->TabIndex = 5;
+			this->cb_EndHour->FormattingEnabled = true;
+			this->cb_EndHour->Location = System::Drawing::Point(290, 272);
+			this->cb_EndHour->Name = L"cb_EndHour";
+			this->cb_EndHour->Size = System::Drawing::Size(75, 23);
+			this->cb_EndHour->TabIndex = 5;
 			// 
-			// comboBox4
+			// cb_EndMin
 			// 
-			this->comboBox4->FormattingEnabled = true;
-			this->comboBox4->Location = System::Drawing::Point(448, 295);
-			this->comboBox4->Name = L"comboBox4";
-			this->comboBox4->Size = System::Drawing::Size(75, 23);
-			this->comboBox4->TabIndex = 6;
+			this->cb_EndMin->FormattingEnabled = true;
+			this->cb_EndMin->Location = System::Drawing::Point(448, 272);
+			this->cb_EndMin->Name = L"cb_EndMin";
+			this->cb_EndMin->Size = System::Drawing::Size(75, 23);
+			this->cb_EndMin->TabIndex = 6;
 			// 
-			// textBox1
+			// tb_NumData
 			// 
-			this->textBox1->Location = System::Drawing::Point(448, 404);
-			this->textBox1->Name = L"textBox1";
-			this->textBox1->Size = System::Drawing::Size(75, 22);
-			this->textBox1->TabIndex = 7;
+			this->tb_NumData->Location = System::Drawing::Point(448, 407);
+			this->tb_NumData->Name = L"tb_NumData";
+			this->tb_NumData->Size = System::Drawing::Size(75, 22);
+			this->tb_NumData->TabIndex = 7;
 			// 
 			// l_Start
 			// 
 			this->l_Start->AutoSize = true;
-			this->l_Start->Location = System::Drawing::Point(287, 164);
+			this->l_Start->Location = System::Drawing::Point(287, 109);
 			this->l_Start->Name = L"l_Start";
 			this->l_Start->Size = System::Drawing::Size(67, 15);
 			this->l_Start->TabIndex = 8;
@@ -179,34 +187,34 @@ namespace Project3 {
 			// l_End
 			// 
 			this->l_End->AutoSize = true;
-			this->l_End->Location = System::Drawing::Point(287, 264);
+			this->l_End->Location = System::Drawing::Point(287, 245);
 			this->l_End->Name = L"l_End";
 			this->l_End->Size = System::Drawing::Size(67, 15);
 			this->l_End->TabIndex = 9;
 			this->l_End->Text = L"終了時刻";
 			// 
-			// l_NumPeople
-			// 
-			this->l_NumPeople->AutoSize = true;
-			this->l_NumPeople->Location = System::Drawing::Point(287, 386);
-			this->l_NumPeople->Name = L"l_NumPeople";
-			this->l_NumPeople->Size = System::Drawing::Size(67, 15);
-			this->l_NumPeople->TabIndex = 10;
-			this->l_NumPeople->Text = L"使用人数";
-			// 
 			// l_Num
 			// 
 			this->l_Num->AutoSize = true;
-			this->l_Num->Location = System::Drawing::Point(529, 407);
+			this->l_Num->Location = System::Drawing::Point(445, 389);
 			this->l_Num->Name = L"l_Num";
-			this->l_Num->Size = System::Drawing::Size(22, 15);
-			this->l_Num->TabIndex = 11;
-			this->l_Num->Text = L"人";
+			this->l_Num->Size = System::Drawing::Size(67, 15);
+			this->l_Num->TabIndex = 10;
+			this->l_Num->Text = L"使用人数";
+			// 
+			// l_NumData
+			// 
+			this->l_NumData->AutoSize = true;
+			this->l_NumData->Location = System::Drawing::Point(529, 410);
+			this->l_NumData->Name = L"l_NumData";
+			this->l_NumData->Size = System::Drawing::Size(22, 15);
+			this->l_NumData->TabIndex = 11;
+			this->l_NumData->Text = L"人";
 			// 
 			// l_EndMin
 			// 
 			this->l_EndMin->AutoSize = true;
-			this->l_EndMin->Location = System::Drawing::Point(529, 298);
+			this->l_EndMin->Location = System::Drawing::Point(529, 275);
 			this->l_EndMin->Name = L"l_EndMin";
 			this->l_EndMin->Size = System::Drawing::Size(22, 15);
 			this->l_EndMin->TabIndex = 12;
@@ -215,7 +223,7 @@ namespace Project3 {
 			// l_StartMin
 			// 
 			this->l_StartMin->AutoSize = true;
-			this->l_StartMin->Location = System::Drawing::Point(529, 195);
+			this->l_StartMin->Location = System::Drawing::Point(529, 138);
 			this->l_StartMin->Name = L"l_StartMin";
 			this->l_StartMin->Size = System::Drawing::Size(22, 15);
 			this->l_StartMin->TabIndex = 13;
@@ -224,7 +232,7 @@ namespace Project3 {
 			// l_StartHour
 			// 
 			this->l_StartHour->AutoSize = true;
-			this->l_StartHour->Location = System::Drawing::Point(387, 195);
+			this->l_StartHour->Location = System::Drawing::Point(371, 139);
 			this->l_StartHour->Name = L"l_StartHour";
 			this->l_StartHour->Size = System::Drawing::Size(22, 15);
 			this->l_StartHour->TabIndex = 14;
@@ -233,7 +241,7 @@ namespace Project3 {
 			// l_EndHour
 			// 
 			this->l_EndHour->AutoSize = true;
-			this->l_EndHour->Location = System::Drawing::Point(387, 298);
+			this->l_EndHour->Location = System::Drawing::Point(371, 280);
 			this->l_EndHour->Name = L"l_EndHour";
 			this->l_EndHour->Size = System::Drawing::Size(22, 15);
 			this->l_EndHour->TabIndex = 15;
@@ -241,7 +249,7 @@ namespace Project3 {
 			// 
 			// b_Search
 			// 
-			this->b_Search->Location = System::Drawing::Point(375, 479);
+			this->b_Search->Location = System::Drawing::Point(241, 467);
 			this->b_Search->Name = L"b_Search";
 			this->b_Search->Size = System::Drawing::Size(98, 40);
 			this->b_Search->TabIndex = 16;
@@ -251,7 +259,7 @@ namespace Project3 {
 			// l_Date
 			// 
 			this->l_Date->AutoSize = true;
-			this->l_Date->Location = System::Drawing::Point(12, 164);
+			this->l_Date->Location = System::Drawing::Point(12, 109);
 			this->l_Date->Name = L"l_Date";
 			this->l_Date->Size = System::Drawing::Size(52, 15);
 			this->l_Date->TabIndex = 17;
@@ -268,23 +276,27 @@ namespace Project3 {
 			this->Controls->Add(this->l_StartHour);
 			this->Controls->Add(this->l_StartMin);
 			this->Controls->Add(this->l_EndMin);
+			this->Controls->Add(this->l_NumData);
 			this->Controls->Add(this->l_Num);
-			this->Controls->Add(this->l_NumPeople);
 			this->Controls->Add(this->l_End);
 			this->Controls->Add(this->l_Start);
-			this->Controls->Add(this->textBox1);
-			this->Controls->Add(this->comboBox4);
-			this->Controls->Add(this->comboBox3);
+			this->Controls->Add(this->tb_NumData);
+			this->Controls->Add(this->cb_EndMin);
+			this->Controls->Add(this->cb_EndHour);
 			this->Controls->Add(this->cb_StartMin);
 			this->Controls->Add(this->cb_StartHour);
 			this->Controls->Add(this->b_SearchReturn);
 			this->Controls->Add(this->dtp_Calender);
 			this->Name = L"SearchForm";
-			this->Text = L"SearchForm";
+			this->Text = L"検索ページ";
 			this->ResumeLayout(false);
 			this->PerformLayout();
 
 		}
 #pragma endregion
-	};
+
+private: System::Void dtp_Calender_ValueChanged(System::Object^ sender, System::EventArgs^ e) {
+
+}
+};
 }
