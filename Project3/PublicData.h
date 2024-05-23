4494aa9ebe68ada1dca1bf9339d82ea0177fc46d
河野涼 <kawano-ry@tounichi-g.co.jp>
2024-05-23 13:13:25 +0900

    変数定義の確定

--- Project3/PublicData.h
+++ Project3/PublicData.h
@@ -4,7 +4,13 @@
 using namespace std;
 /*static string Name;
 */
+static int Id = 0;			//ユーザー名
+static int flag = 1;		//会員・非会員フラグ
+static int RoomNumber = 0;	//選択部屋番号
 
-static int flag = 1;
-static int RoomNumber = 0;
-static int StartHour = 0;
+static int StartHour = 0;	//予約開始時
+static int StartMIn = 0;	//予約開始分
+
+static int EndHour = 0;		//予約終了時
+static int EndMIn = 0;		//予約終了分
+static int Num = 0;			//人数
