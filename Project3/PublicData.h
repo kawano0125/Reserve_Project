a82e5bb09fb02b552688e66b7c0f8d6634d4a7b3
河野涼 <kawano-ry@tounichi-g.co.jp>
2024-05-23 12:36:24 +0900

    変数定義の追加

---
+++ Project3/PublicData.h
@@ -0,0 +1,10 @@
+#pragma once
+#include<iostream>
+#include<string> //stringクラスを使用可能にする
+using namespace std;
+/*static string Name;
+*/
+
+static int flag = 1;
+static int RoomNumber = 0;
+static int StartHour = 0;
