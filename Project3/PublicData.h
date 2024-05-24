c3cc46b97cf86e04f3e5789f339a1b413488bcbd
松井俊樹 <matsui-ts@tounichi-g.co.jp>
2024-05-24 13:15:26 +0900

    処理追加　松井

--- Project3/PublicData.h
+++ Project3/PublicData.h
@@ -9,8 +9,8 @@ static int flag = 1;		//会員・非会員フラグ
 static int RoomNumber = 0;	//選択部屋番号
 
 static int StartHour = 0;	//予約開始時
-static int StartMIn = 0;	//予約開始分
+static int StartMin = 0;	//予約開始分
 
 static int EndHour = 0;		//予約終了時
-static int EndMIn = 0;		//予約終了分
+static int EndMin = 0;		//予約終了分
 static int Num = 0;			//人数
